6f310d1c2fbe7bf43bf2161945d33b4fa3959a1c
Anders Åkerö <anders.akero@motionpicturesolutions.com>
2018-05-07 15:20:03 +0100

    Checking min and max values

--- src/Game.php
+++ src/Game.php
@@ -6,5 +6,52 @@
  */
 final class Game
 {
+    /**
+     * Lowest and highest value the game will iterate between
+     */
+    const MIN = 0;
+    const MAX = 100;
 
+    /**
+     * @var int
+     */
+    private $start = self::MIN;
+    /**
+     * @var int
+     */
+    private $stop = self::MAX;
+
+    /**
+     * Game constructor.
+     *
+     * @param int $start
+     * @param int $stop
+     */
+    public function __construct(int $start = self::MIN, int $stop = self::MAX)
+    {
+        $this->assertValidStart($start);
+        $this->assertValidStop($stop);
+        $this->start = $start;
+        $this->stop = $stop;
+    }
+
+    /**
+     * @param int $start
+     */
+    private function assertValidStart(int $start): void
+    {
+        if ($start < self::MIN) {
+            throw new InvalidArgumentException(sprintf('Can not be less then %s', self::MIN));
+        }
+    }
+
+    /**
+     * @param int $stop
+     */
+    private function assertValidStop(int $stop): void
+    {
+        if ($stop > self::MAX) {
+            throw new InvalidArgumentException(sprintf('Can not be higher then %s', self::MAX));
+        }
+    }
 }
--- tests/GameTest.php
+++ tests/GameTest.php
@@ -7,11 +7,35 @@ final class GameTest extends TestCase
 {
 
     /**
-     * Making sure that we can load the class
+     * Making sure that we can load the class wit
      */
-    public function test__construct()
+    public function testConstructDefault(): void
     {
         new Game;
         $this->assertTrue(true);
+    }
+
+    /**
+     * @dataProvider invalidConstructData
+     *
+     * @param $start
+     * @param $stop
+     *
+     * @expectedException InvalidArgumentException
+     */
+    public function testConstructInvalid($start, $stop): void
+    {
+        new Game($start, $stop);
+    }
+
+    /**
+     * @return array
+     */
+    public function invalidConstructData(): array
+    {
+        return [
+            'Can not be less then min'   => ['start' => Game::MIN - 1, 'stop' => Game::MAX],
+            'Can not be higher then max' => ['start' => Game::MIN, 'stop' => Game::MAX + 1],
+        ];
     }
 }
