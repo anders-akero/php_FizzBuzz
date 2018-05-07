ac543199cda447569cf9d78c337797f4cd55279c
Anders Åkerö <anders.akero@motionpicturesolutions.com>
2018-05-07 17:09:32 +0100

    Possible to use via browser

--- README.md
+++ README.md
@@ -11,7 +11,12 @@ Dependencies (to run with Docker):
 Initialise and start all the containers:
 * `docker-compose up -d`
 
-## Run Unit-tests ##
+### In browser ###
+`http://localhost:5050/X/Y` Where X is the start value and Y is the end value.
+
+Example: `http://localhost:5050/1/100`
+
+### Unit-tests ###
 Make sure that the containers are up and running, then run the following:
 
 `docker-compose exec php-fpm ./vendor/bin/phpunit --bootstrap vendor/autoload.php tests --testdox`
--- public/index.php
+++ public/index.php
@@ -1,3 +1,23 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+
+$delimiter = '/';
+$input = explode($delimiter, $_SERVER['REQUEST_URI']);
+$start = $input[1] ?? Game::MIN;
+$stop = $input[2] ?? Game::MAX;
+try {
+    $result = (new Game($start, $stop))->result();
+    echo implode('<br>', $result);
+} catch (InvalidArgumentException $e) {
+    echo $e->getMessage();
+} catch (Error $error) {
+    echo '<b>Invalid input.</b><br>';
+    echo sprintf('Please make sure that you use format X%sY where X is the desired start value and Y is the stop value.<br>', $delimiter);
+    echo sprintf('For example: %s/%d%s%d',
+        $_SERVER['HTTP_HOST'],
+        Game::MIN,
+        $delimiter,
+        Game::MAX
+    );
+}
